df139584b3e6dd7e8b24f3b49d744314317f9bb7
SMTBVH2 <vietngovan0905@gmail.com>
2026-04-17 14:18:32 +0700

    code giao dien moi

--- src/main/java/com/example/controllers/AdminControllers/BillManagementController.java
+++ src/main/java/com/example/controllers/AdminControllers/BillManagementController.java
@@ -1,4 +1,21 @@
 package com.example.controllers.AdminControllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+
 public class BillManagementController {
+
+    @FXML private ComboBox<String> statusCombo;
+    @FXML private DatePicker fromDate;
+    @FXML private DatePicker toDate;
+    @FXML private TableView<?> billTable;
+
+    public void initialize() {
+        // Gán danh sách trạng thái cho ComboBox
+        statusCombo.getItems().setAll("Tất cả", "Đã thanh toán", "Chờ thanh toán", "Quá hạn");
+        // Bạn có thể chọn giá trị mặc định
+        statusCombo.setValue("Tất cả");
+    }
 }
