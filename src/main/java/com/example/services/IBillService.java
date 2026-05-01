6d0df49ab97d5f0410e9333b07bd25b8d37e33c3
TuanPham <tuan080706@gmail.com>
2026-05-01 18:33:12 +0700

    update form

--- src/main/java/com/example/services/IBillService.java
+++
@@ -1,9 +0,0 @@
-package com.example.services;
-
-import com.example.DTO.RecentBill;
-
-import java.util.List;
-
-public interface IBillService {
-    public List<RecentBill> getRecentBills(int limit);
-}
--- src/main/java/com/example/services/IRenevueService.java
+++
@@ -1,9 +0,0 @@
-package com.example.services;
-
-import javafx.scene.chart.XYChart;
-
-public interface IRenevueService {
-    public XYChart.Series<String, Number> getYearRevenueData();
-    public XYChart.Series<String, Number> getMonthRevenueData();
-    public XYChart.Series<String, Number> getWeekRevenueData();
-}
